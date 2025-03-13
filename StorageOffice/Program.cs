c6bce87408713e0a084187f7f3def546e73df061
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-03-13 17:34:31 +0100

    create an empty c# console project

---
+++ StorageOffice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StorageOffice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
