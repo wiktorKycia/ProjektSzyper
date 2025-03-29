7c2780e9e231597ea07c2003566cb5044996760c
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-03-29 18:16:49 +0100

    move option classes into a new file

--- StorageOffice/classes/CLI/Select.cs
+++ StorageOffice/classes/CLI/Select.cs
@@ -2,19 +2,6 @@ using System;
 
 namespace StorageOffice.classes.CLI;
 
-internal interface ISelectable
-{
-    public void Toggle();
-    public string ToString();
-}
-
-internal interface IHighlightable
-{
-    public void ToggleHighlight();
-    public string ToString();
-}
-
-
 public abstract class Select
 {
     protected abstract int CurrentIndex { get; set; } 
@@ -53,53 +40,7 @@ public class RadioSelect(List<RadioOption> options) : Select
 
 
 
-public class RadioOption: ISelectable
-{
-    public string Text { get; set; }
-    public bool IsSelected { get; set; } = false;
-    public char SpecialSign { get; set; }
-    
 
-    public RadioOption(string text, char specialSign = '\u2022')
-    {
-        Text = text;
-        SpecialSign = specialSign;
-    }
-    public void Toggle()
-    {
-        IsSelected = !IsSelected;
-    }
-    public override string ToString()
-    {
-        return $"[{SpecialSign}] {Text}";
-    }
-}
-
-public class CheckBoxOption: ISelectable, IHighlightable
-{
-    public string Text { get; set; }
-    public bool IsSelected { get; set; } = false;
-    public char SpecialSign { get; set; }
-    public bool IsHighlighted { get; set; } = false;
-
-    public CheckBoxOption(string text, char specialSign = '\u2713')
-    {
-        Text = text;
-        SpecialSign = specialSign;
-    }
-    public void Toggle()
-    {
-        IsSelected = !IsSelected;
-    }
-    public void ToggleHighlight()
-    {
-        IsHighlighted = !IsHighlighted;
-    }
-    public override string ToString()
-    {
-        return $"[{SpecialSign}] {Text}";
-    }
-}
 
 /*
 public class Select
