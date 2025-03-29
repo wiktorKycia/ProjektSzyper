be4201da4cfe429150a2f6c19edf533ad6bc6b16
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-03-29 18:55:28 +0100

    clear out the main program

--- StorageOffice/Program.cs
+++ StorageOffice/Program.cs
@@ -5,42 +5,7 @@ namespace StorageOffice
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            CheckBoxSelect select = new(new List<Option>
-            {
-                new Option("Option 1"),
-                new Option("Option 2"),
-                new Option("Option 3")
-            });
-            while(true)
-            {
-                Console.Clear();
-                ConsoleOutput.PrintColorMessage("Select an option:\n\n", ConsoleColor.Cyan);
-                select.DisplayOptions();
-                Console.WriteLine("Press Up/Down to navigate, Enter to select, Esc to exit.");
-
-                ConsoleKey key = ConsoleInput.GetConsoleKey();
-                if (key == ConsoleKey.UpArrow)
-                {
-                    select.MoveUp();
-                }
-                else if (key == ConsoleKey.DownArrow)
-                {
-                    select.MoveDown();
-                }
-                else if (key == ConsoleKey.Enter)
-                {
-                    select.SelectOption();
-                }
-                else if (key == ConsoleKey.Escape)
-                {
-                    break;
-                }
-                else
-                {
-                    ConsoleOutput.PrintColorMessage("Invalid key. Please use Up/Down to navigate.", ConsoleColor.Red);
-                }
-            }
+            
         }
     }
 }
