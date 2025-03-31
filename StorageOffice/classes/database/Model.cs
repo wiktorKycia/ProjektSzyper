ec1e37c468b221abffbc98cc7ffd39cfd513f596
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-03-31 13:08:52 +0200

    update models and create an AppContext

--- StorageOffice/classes/database/Model.cs
+++ StorageOffice/classes/database/Model.cs
@@ -5,7 +5,24 @@ using System.Collections.Generic;
 
 public class AppContext : DbContext
 {
+    public DbSet<Product> Products {get;set;}
+    public DbSet<Transaction> Transactions {get;set;}
 
+    public string DbPath {get;}
+
+    public AppContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite("FileName=StorageOffice.db", opt =>
+        {
+            opt.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+        });
+    }
 }
 
 
@@ -15,13 +32,16 @@ public class Product
     public string Name {get;set;}
     public int Quantity {get;set;}
     public string Unit {get;set;}
+
+    public List<Transaction> Transactions {get;set;}
 }
 
 public class Transaction
 {
     public int Id {get;set;}
     public string SubjectName {get;set;}
-    public List<Product> Products {get;set;}
+    public int ProuctId {get;set;}
+    public Product Product {get;set;}
     public DateTime Date {get;set;}
     public int Quantity {get;set;}
     public string Unit {get;set;}
