f313da635b27f40f83d977cf244b1f9c555e60ef
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-04-06 10:37:11 +0200

    Add CRUD methods for Product management in Database.cs

--- StorageOffice/classes/database/Database.cs
+++ StorageOffice/classes/database/Database.cs
@@ -18,5 +18,37 @@ namespace StorageOffice.classes.database
             _context.Database.Migrate();
         }
         // tu piszemy metody do dodawania, usuwania i edytowania danych w bazie
+
+        public void AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+
+        public void UpdateProduct(int productId, Product updatedProduct)
+        {
+            var existingProduct = _context.Products.Find(productId);
+            if (existingProduct != null)
+            {
+                existingProduct.Name = updatedProduct.Name;
+                existingProduct.Quantity = updatedProduct.Quantity;
+                existingProduct.Unit = updatedProduct.Unit;
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            var product = _context.Products.Find(productId);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
+
+        public List<Product> GetAllProducts() => [.. _context.Products];
+
+        public Product? GetProductById(int productId) => _context.Products.Find(productId);
     }
 }
