24b475c75930e04e98bdec9c04deb6b27731e5e7
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-04-08 18:27:47 +0200

    add a method to seed data in Database class, remove other methods

--- StorageOffice/classes/database/Database.cs
+++ StorageOffice/classes/database/Database.cs
@@ -19,36 +19,7 @@ namespace StorageOffice.classes.database
         }
         // tu piszemy metody do dodawania, usuwania i edytowania danych w bazie
 
-        public void AddProduct(Product product)
-        {
-            _context.Products.Add(product);
-            _context.SaveChanges();
-        }
+        public void SeedData() => DataSeeder.Seed(_context);
 
-        public void UpdateProduct(int productId, Product updatedProduct)
-        {
-            var existingProduct = _context.Products.Find(productId);
-            if (existingProduct != null)
-            {
-                existingProduct.Name = updatedProduct.Name;
-                existingProduct.Quantity = updatedProduct.Quantity;
-                existingProduct.Unit = updatedProduct.Unit;
-                _context.SaveChanges();
-            }
-        }
-
-        public void DeleteProduct(int productId)
-        {
-            var product = _context.Products.Find(productId);
-            if (product != null)
-            {
-                _context.Products.Remove(product);
-                _context.SaveChanges();
-            }
-        }
-
-        public List<Product> GetAllProducts() => [.. _context.Products];
-
-        public Product? GetProductById(int productId) => _context.Products.Find(productId);
     }
 }
