03950fc2948686fc8cd810a3faf30443f6b4a05f
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-04-17 17:06:50 +0200

    implement the logic of getting the user string

--- StorageOffice/classes/CLI/Input.cs
+++ StorageOffice/classes/CLI/Input.cs
@@ -5,6 +5,23 @@ public static partial class ConsoleInput
 {
     public static string GetUserString(string message)
     {
-                
+        string result = "";
+        while(result == "")
+        {
+            try
+            {
+                Console.Write(message);
+                result = Console.ReadLine() ?? "";
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.Write(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+        return result;
     }
 }
