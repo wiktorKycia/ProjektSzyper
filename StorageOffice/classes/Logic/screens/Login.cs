a47fca410052fa750a3a38d1e7ec916601bc2360
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-04-17 20:54:39 +0200

    copy the code and paste it in Login class, change some values and call different methods at the end

--- StorageOffice/classes/Logic/screens/Login.cs
+++ StorageOffice/classes/Logic/screens/Login.cs
@@ -1,5 +1,9 @@
 using System;
 using StorageOffice.classes.CLI;
+using StorageOffice.classes.LogServices;
+using StorageOffice.classes.UsersManagement.Modules;
+using StorageOffice.classes.UsersManagement.Services;
+
 namespace StorageOffice.classes.Logic;
 
 public class Login
@@ -14,16 +18,11 @@ public class Login
         _title = title;
         _heading = heading;
         _keyboardActions = new Dictionary<ConsoleKey, KeyboardAction>(){
-            { ConsoleKey.UpArrow, select.MoveUp },
-            { ConsoleKey.DownArrow, select.MoveDown },
-            { ConsoleKey.Enter, select.InvokeOperation },
             { ConsoleKey.Escape, () => Environment.Exit(0) }
         };
         _displayKeyboardActions = new Dictionary<string, string>(){
-            { "\u2191", "move up" },
-            { "\u2193", "move down" },
-            { "<Enter>", "select" },
-            { "<Esc>", "exit" }
+            { "<Esc>", "exit" },
+            { "Any other key", "enter username and password" }
         };
         Run();
     }
@@ -39,6 +38,83 @@ public class Login
             {
                 _keyboardActions[key]();
             }
+            else
+            {
+                User user = new User();
+                GetUsername(user);
+                string password = GetPassword();
+
+                Role? role = PasswordManager.VerifyPasswordAndGetRole(user.Username, password);
+                if (role == null)
+                {
+                    Console.WriteLine("Username or password is incorrect. Press any key and try again");
+                    Console.ReadKey();
+                    continue;
+                }
+                else
+                {
+                    user.Role = (Role)role;
+                }
+            }
+        }
+    }
+    private void GetUsername(User user)
+    {
+        bool isCorrect = false;
+        while(!isCorrect)
+        {
+            try
+            {
+                user.Username = ConsoleInput.GetUserString("Enter your username: ");
+                isCorrect = true;
+            }
+            catch (ArgumentNullException e)
+            {
+                ConsoleOutput.PrintColorMessage(e.Message + "\n", ConsoleColor.Red);
+                Console.WriteLine("Press any key to try again...");
+                ConsoleInput.WaitForAnyKey();
+            }
+            catch (ArgumentException e)
+            {
+                ConsoleOutput.PrintColorMessage(e.Message + "\n", ConsoleColor.Red);
+                Console.WriteLine("Press any key to try again...");
+                ConsoleInput.WaitForAnyKey();
+            }
+            catch (Exception e)
+            {
+                ConsoleOutput.PrintColorMessage($"An unexpected error occurred: {e.Message}\n", ConsoleColor.Red);
+                Console.WriteLine("Press any key to try again...");
+                ConsoleInput.WaitForAnyKey();
+            }
+        }
+    }
+    private string GetPassword()
+    {
+        while(true)
+        {
+            try
+            {
+                string password = ConsoleInput.GetUserString("Enter your password: ");
+                return password;
+            }
+            catch (ArgumentNullException e)
+            {
+                ConsoleOutput.PrintColorMessage(e.Message + "\n", ConsoleColor.Red);
+                Console.WriteLine("Press any key to try again...");
+                ConsoleInput.WaitForAnyKey();
+            }
+            catch (ArgumentException e)
+            {
+                ConsoleOutput.PrintColorMessage(e.Message + "\n", ConsoleColor.Red);
+                Console.WriteLine("Press any key to try again...");
+                ConsoleInput.WaitForAnyKey();
+            }
+            catch (Exception e)
+            {
+                ConsoleOutput.PrintColorMessage($"An unexpected error occurred: {e.Message}\n", ConsoleColor.Red);
+                Console.WriteLine("Press any key to try again...");
+                ConsoleInput.WaitForAnyKey();
+            }
         }
     }
 
@@ -47,23 +123,14 @@ public class Login
         Console.Clear();
         string content = _heading;
         
-        if (_select.Options != null)
-        {
-            foreach (var option in _select.Options)
-            {
-                content += "\n" + ConsoleOutput.CenteredText(option?.ToString() ?? "[ ] No Text", true);
-            }
-        }
-        else
-        {
-            content += "\n" + ConsoleOutput.CenteredText("No options available", true);
-        }
-        
         Console.WriteLine(ConsoleOutput.UIFrame(_title, content));
 
         foreach (var action in _displayKeyboardActions)
         {
             Console.WriteLine($"{action.Key} - {action.Value}");
         }
+
+        ConsoleOutput.HorizontalLine('-');
+
     }
 }
