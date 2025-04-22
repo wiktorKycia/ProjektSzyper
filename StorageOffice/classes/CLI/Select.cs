042e5fdf289d5eba6ca788f13435ec51afcc0294
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-04-22 18:29:55 +0200

    add docstrings in Select.cs file

--- StorageOffice/classes/CLI/Select.cs
+++ StorageOffice/classes/CLI/Select.cs
@@ -2,15 +2,45 @@ using System;
 
 namespace StorageOffice.classes.CLI;
 
+/// <summary>
+/// Abstract base class for selection UI components that manage selectable options.
+/// </summary>
 public abstract class Select
 {
+    /// <summary>
+    /// Gets or sets the index of the currently selected option.
+    /// </summary>
     protected abstract int CurrentIndex { get; set; }
+
+    /// <summary>
+    /// Gets the collection of selectable options available in this selection component.
+    /// </summary>
     public IEnumerable<ISelectable> Options => GetOptions();
 
+    /// <summary>
+    /// Gets the collection of selectable options for this select component.
+    /// </summary>
+    /// <returns>A collection of ISelectable objects representing the available options.</returns>
     protected abstract IEnumerable<ISelectable> GetOptions();
+
+    /// <summary>
+    /// Selects the currently highlighted option.
+    /// </summary>    
     public abstract void SelectOption();
+
+    /// <summary>
+    /// Moves the selection highlight up one option in the list.
+    /// </summary>
     public abstract void MoveUp();
+
+    /// <summary>
+    /// Moves the selection highlight down one option in the list.
+    /// </summary>
     public abstract void MoveDown();
+
+    /// <summary>
+    /// Invokes the operation associated with the currently selected option.
+    /// </summary>
     public abstract void InvokeOperation();
 }
 
